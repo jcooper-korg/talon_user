fdf7f58ecbd3d4e0663809a614c1835700bab639
John Cooper <jcooper@korgrd.com>
2018-07-27 10:06:43 -0700

    changed  'front' to 'exposay'

--- mission_control.py
+++ mission_control.py
@@ -7,7 +7,7 @@ ctx = Context('mission_control')
 
 ctx.keymap({
     "(mission control | troll up)": lambda m: macos.dock_notify('com.apple.expose.awake'),
-    "(front | troll down)": lambda m: macos.dock_notify('com.apple.expose.front.awake'),
+    "(expozay | troll down)": lambda m: macos.dock_notify('com.apple.expose.front.awake'),		# jsc changed  'front' to 'exposay'
     "(next space | troll left)": Key('ctrl-alt-cmd-left'),
     "(last space | troll right)": Key('ctrl-alt-cmd-right'),
     "launchpad": lambda m: macos.dock_notify('com.apple.launchpad.toggle'),
