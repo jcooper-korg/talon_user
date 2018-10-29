e0e8fbe872d70576989b8dc41daac4677c5dc2bf
John Cooper <jcooper@korgrd.com>
2018-10-29 08:50:32 -0700

    update from master examples

--- switcher.py
+++ switcher.py
@@ -3,20 +3,21 @@
 
 from talon.voice import Word, Context, Key, Rep, Str, press
 from talon import ui
+import time
 
 apps = {}
 
-def switch_app(m, name=None):		# jsc modified
+def switch_app(m, name=None):		# jsc modified for single-word shortcut
     if name is None:
         name = str(m._words[1])
     full = apps.get(name)
-    if not full:
-        return
+    if not full: return
     for app in ui.apps():
         if app.name == full:
             app.focus()
+            # TODO: replace sleep with a check to see when it is in foreground
+            time.sleep(0.25)
             break
-
 
 ctx = Context('switcher')
 keymap = {
@@ -36,6 +37,8 @@ def update_lists():
     global apps
     new = {}
     for app in ui.apps():
+        if app.background and not app.windows():
+            continue
         words = app.name.split(' ')
         for word in words:
             if word and not word in new:
@@ -46,11 +49,8 @@ def update_lists():
     ctx.set_list('apps', new.keys())
     apps = new
 
-
 def ui_event(event, arg):
-    if event in ('app_activate', 'app_deactivate', 'app_launch', 'app_close'):
-        update_lists()
-
+    update_lists()
 
 ui.register('', ui_event)
 update_lists()
