6b80f8ffbcff4ef96f2b8a11055f3768d03305fe
John Cooper <jcooper@korgrd.com>
2018-10-29 10:24:20 -0700

    add additional commands

--- smartgit.py
+++ smartgit.py
@@ -6,6 +6,14 @@ from talon.voice import Key, Context
 ctx = Context('smartgit', bundle='com.syntevo.smartgit')
 ctx.keymap({
     'commit changes': Key('cmd-k'),
+    'undo last commit': Key('shift-cmd-k'),
 	'pull it': Key('cmd-p'),
 	'push it': Key('cmd-u'),
+	'show log': Key('cmd-l'),
+	'(edit commit message | edit message | change message)': Key('f2'),
+	'check out branch': Key('cmd-g'),
+	'add branch': Key('f7'),
+	'stage': Key('cmd-t'),
+	'unstage': Key('shift-cmd-t'),
+	'(show | hide) unchanged files': Key('cmd-1'),
   })
